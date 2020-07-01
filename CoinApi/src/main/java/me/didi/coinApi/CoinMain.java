f65f69db7266daf35bd6b357af9732a4c77589cb
didi1150 <didicomputing@gmail.com>
2020-07-01 08:19:49 +0200

    0.0.6 Update

--- CoinApi/src/main/java/me/didi/coinApi/CoinMain.java
+++ CoinApi/src/main/java/me/didi/coinApi/CoinMain.java
@@ -1,9 +1,16 @@
 package me.didi.coinApi;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import me.didi.coinApi.mysql.MySQL;
 
-public class CoinMain {
+public class CoinMain extends JavaPlugin {
+	File file = new File("plugins/CoinApi", "config.yml");
+	FileConfiguration cfg = this.getConfig();
 
 	public void disconnect() {
 		MySQL.disconnect();
@@ -11,5 +18,27 @@ public class CoinMain {
 
 	public void connect() {
 		MySQL.connect();
+	}
+
+	public void init() {
+		cfg.addDefault("username", "root");
+		cfg.addDefault("password", "");
+		cfg.addDefault("database", "onlinetime");
+		cfg.addDefault("port", "3306");
+		cfg.addDefault("host", "localhost");
+		saveCfg();
+		MySQL.username = cfg.getString("username");
+		MySQL.password = cfg.getString("password");
+		MySQL.database = cfg.getString("database");
+		MySQL.host = cfg.getString("host");
+		MySQL.port = cfg.getString("port");
+	}
+
+	private void saveCfg() {
+		try {
+			cfg.save(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
