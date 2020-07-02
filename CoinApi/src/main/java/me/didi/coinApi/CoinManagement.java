40ab429ed128fdbbc3ec9771baf7bac399706509
didi1150 <didicomputing@gmail.com>
2020-07-02 20:24:19 +0200

    1.2 Update

--- CoinApi/src/main/java/me/didi/coinApi/CoinManagement.java
+++ CoinApi/src/main/java/me/didi/coinApi/CoinManagement.java
@@ -2,17 +2,21 @@ package me.didi.coinApi;
 
 import me.didi.coinApi.mysql.MySQL;
 
-public class CoinManagement {
+public class CoinManagement
+{
 
 	public static CoinManagement instance = new CoinManagement();
 
-	public static CoinManagement getInstance() {
+	public static CoinManagement getInstance()
+	{
 		return instance;
 	}
 
-	public void addCoins(String uuid, int amount) {
+	public void addCoins(String uuid, int amount)
+	{
 		int coinsBefore = 0;
-		if (MySQL.getValue(uuid) != null) {
+		if (MySQL.getValue(uuid) != null)
+		{
 			coinsBefore = Integer.parseInt(MySQL.getValue(uuid));
 		}
 
@@ -20,13 +24,20 @@ public class CoinManagement {
 		MySQL.setValue(String.valueOf(coinsAfter), uuid);
 	}
 
-	public void removeCoins(String uuid, int amount) {
+	public void removeCoins(String uuid, int amount)
+	{
 		int coinsBefore = 0;
-		if (MySQL.getValue(uuid) != null) {
+		if (MySQL.getValue(uuid) != null)
+		{
 			coinsBefore = Integer.parseInt(MySQL.getValue(uuid));
 		}
 
 		int coinsAfter = coinsBefore - amount;
 		MySQL.setValue(String.valueOf(coinsAfter), uuid);
+	}
+
+	public int getCoins(String uuid)
+	{
+		return Integer.parseInt(MySQL.getValue(uuid));
 	}
 }
