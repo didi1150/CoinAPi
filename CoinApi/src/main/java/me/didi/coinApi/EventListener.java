77172242604690fc4d80ced0a94a2eace094f35c
didi1150 <didicomputing@gmail.com>
2020-07-08 19:05:08 +0200

    1.6

---
+++ CoinApi/src/main/java/me/didi/coinApi/EventListener.java
@@ -0,0 +1,16 @@
+package me.didi.coinApi;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import me.didi.coinApi.events.CoinChangeEvent;
+
+public class EventListener implements Listener
+{
+
+	@EventHandler
+	public void onCoinChange(CoinChangeEvent e) {
+		
+	}
+	
+}
